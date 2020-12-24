c8e1ef11b27f44a10b847eb93a03fac658355748
Daniel Hart <daniel.hart@uk.clara.net>
2020-12-24 11:37:24 +0000

    property binding intro and comments

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,11 @@ import { Component } from '@angular/core'
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    // vars for string interpolation demo
     serverId = 10;
     serverStatus = 'offline'
 
+    getServerStatus(){
+        return this.serverStatus
+    }
 }
